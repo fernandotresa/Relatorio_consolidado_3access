91236e03112bf47be7ff8fc9c8d07651c389060d
Diego <admin@dbltecnologia.com.br>
2020-02-27 23:18:43 -0300

    .

--- index.js
+++ index.js
@@ -424,13 +424,16 @@ async function popularExcel(result, workbook){
                 let serial_gtw = element.serial_gtw
                 let tipoDeIngresso = nome_tipo_produto.includes("HOSPEDARIA") ? "Hospedaria" : "Ingressos"
 
+                console.log("1", element.id_estoque_utilizavel, element.data_utilizacao, moment(element.data_utilizacao).format("hh:mm:ss"), serial_gtw)
+
+
                 if(!serial_gtw.length || serial_gtw.length === 0)
                     serial_gtw = ""
 
                 if(data_utilizacao.length === 0 || data_utilizacao === '0000-00-00 00:00:00')
                     data_utilizacao = data_log_venda
 
-                console.log("1", element.id_estoque_utilizavel, element.data_utilizacao, moment(element.data_utilizacao).format("hh:mm:ss"), serial_gtw)
+                console.log("2", element.id_estoque_utilizavel, element.data_utilizacao, moment(element.data_utilizacao).format("hh:mm:ss"), serial_gtw)
 
 
                 let data_utilizacao = moment(element.data_utilizacao).format("DD/MM/YYYY")
